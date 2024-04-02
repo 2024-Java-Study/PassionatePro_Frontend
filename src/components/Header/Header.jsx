aebe112772df31b1b3d342ad82d15d5d00a26319
ajeong <ajung7038@naver.com>
2024-04-02 10:22:22 +0900

    Feat: Header 추가 #1

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,36 @@
+import { useNavigate } from "react-router-dom";
 import * as S from "./Header.style";
 
 const Header = () => {
-  return <S.Header>헤더</S.Header>;
+  const navigate = useNavigate();
+
+  const handleNavBarClick = (path) => {
+    navigate(path);
+  };
+
+  return (
+    <S.Header>
+      <S.LeftSection>
+        <S.LeftSectionLogo>
+          <S.Logo>PP</S.Logo>
+        </S.LeftSectionLogo>
+        <S.RightSectionNav>
+          <S.Navbar
+            onClick={() => handleNavBarClick("/")}
+            // style={({ isActive }) => { return (isActive ? "black" : "gray", );}}
+          >
+            홈
+          </S.Navbar>
+          <S.Navbar onClick={() => handleNavBarClick("/boards")}>
+            글쓰기
+          </S.Navbar>
+        </S.RightSectionNav>
+      </S.LeftSection>
+      <S.RightSection>
+        <S.Navbar>로그아웃</S.Navbar>
+      </S.RightSection>
+    </S.Header>
+  );
 };
 
 export default Header;
--- src/components/Header/Header.style.jsx
+++ src/components/Header/Header.style.jsx
@@ -1,3 +1,41 @@
 import styled from "@emotion/styled";
 
-export const Header = styled.div``;
+export const Header = styled.div`
+  display: flex;
+  font-size: 25px;
+  justify-content: space-between;
+  padding-left: 10%;
+  padding-right: 10%;
+`;
+
+export const LeftSection = styled.div`
+  display: flex;
+  width: 30%;
+  justify-content: space-between;
+`;
+export const LeftSectionLogo = styled.div``;
+
+export const Logo = styled.h1`
+  font-family: "Inter-ExtraBold";
+`;
+
+export const RightSectionNav = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+export const Navbar = styled.button`
+  background-color: white;
+  outline: none;
+  border: none;
+  cursor: pointer;
+
+  font-size: 20px;
+  font-family: "Inter-ExtraBold";
+  font-weight: bold;
+`;
+
+export const RightSection = styled.div`
+  display: flex;
+  justify-content: center;
+`;
