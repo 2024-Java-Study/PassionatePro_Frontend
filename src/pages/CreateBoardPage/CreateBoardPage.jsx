9540d22c737ab61af04575a7b6030c18561d1b3f
ajeong <ajung7038@naver.com>
2024-04-26 17:53:22 +0900

    Feat: CreateBoardPage CSS 추가 #4

--- src/pages/CreateBoardPage/CreateBoardPage.jsx
+++ src/pages/CreateBoardPage/CreateBoardPage.jsx
@@ -1,11 +1,48 @@
 import * as S from "./CreateBoardPage.style";
 import { Header, PageLayout } from "../../components";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const CreateBoardPage = () => {
+  const [boardInfo, setBoardInfo] = useState({
+    title: "",
+    content: "",
+  });
+
+  const onChangeInfo = (e) => {
+    setBoardInfo({
+      ...boardInfo,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const navigate = useNavigate();
+  const navigateToHomePage = () => {
+    navigate("/");
+  };
+
   return (
     <S.CreateBoardPage>
       <PageLayout header={<Header />}></PageLayout>
       <S.Line></S.Line>
+      <S.CreateBoardContent>
+        <S.InputTitle
+          name="title"
+          onChange={onChangeInfo}
+          value={boardInfo.title}
+          placeholder="제목을 입력하세요 ..."
+        />
+        <br />
+        <S.InputContent
+          name="content"
+          onChange={onChangeInfo}
+          value={boardInfo.content}
+          placeholder="내용을 입력하세요 ..."
+        />
+        <br />
+        <br />
+        <S.CompleteButton onClick={navigateToHomePage}>작성</S.CompleteButton>
+      </S.CreateBoardContent>
     </S.CreateBoardPage>
   );
 };
--- src/pages/CreateBoardPage/CreateBoardPage.style.jsx
+++ src/pages/CreateBoardPage/CreateBoardPage.style.jsx
@@ -6,3 +6,33 @@ export const Line = styled.div`
   border: 1px solid black;
   width: 100%;
 `;
+
+export const CreateBoardContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 100px;
+`;
+
+export const InputTitle = styled.input`
+  font-size: 30px;
+  font-weight: bold;
+  width: 70%;
+  padding: 10px;
+  border: 1px solid #d9d9d9;
+`;
+
+export const InputContent = styled.textarea`
+  width: 70%;
+  padding: 10px;
+  border: 1px solid #d9d9d9;
+`;
+
+export const CompleteButton = styled.button`
+  width: 70%;
+  padding: 10px;
+  background-color: #7e7e7e;
+  color: white;
+  border: none;
+  cursor: pointer;
+`;
