e7ab57271886f131256557991d235b8e6fe3eb44
ajeong <ajung7038@naver.com>
2024-08-06 18:12:36 +0900

    Del: Redux 삭제 #27

--- src/index.js
+++ src/index.js
@@ -3,21 +3,11 @@ import ReactDOM from "react-dom/client";
 import router from "./router";
 import { RouterProvider } from "react-router-dom";
 import "./index.css";
-import { CookiesProvider } from "react-cookie";
-// import registerServiceWorker from "./registerServiceWorker";
-import { createStore } from "redux";
-import rootReducer from "./store/modules";
-import PrivateRoute from "./PrivateRoute";
-
-const store = createStore(rootReducer);
-console.log(store.getState());
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 // registerServiceWorker();
 root.render(
-  <CookiesProvider>
     <React.StrictMode>
       <RouterProvider router={router} />
     </React.StrictMode>
-  </CookiesProvider>
 );
